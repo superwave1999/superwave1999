20c3914fbfbdff46c6b71c8285f672a3f5780912
Imanol Romera <imanolrrl99@gmail.com>
2023-08-21 17:24:12 +0200

    No square photo, fix bg animation in chromium

--- components/TopCard.vue
+++ components/TopCard.vue
@@ -23,6 +23,32 @@ div.card {
 img {
   width: 100%;
   object-fit: contain;
+  animation-duration: 30s;
+  animation-name: animimg;
+  animation-direction: normal;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes animimg {
+  0% {
+    border-radius: 40% 60% 70% 30% / 46% 30% 70% 54%;
+  }
+  20% {
+    border-radius: 66% 34% 53% 47% / 44% 59% 41% 56%;
+  }
+  40% {
+    border-radius: 56% 44% 44% 56% / 68% 49% 51% 32%;
+  }
+  60% {
+    border-radius: 50% 50% 48% 52% / 48% 61% 39% 52%;
+  }
+  80% {
+    border-radius: 61% 39% 27% 73% / 67% 40% 60% 33%;
+  }
+  100% {
+    border-radius: 40% 60% 70% 30% / 46% 30% 70% 54%;
+  }
 }
 
 @media (min-width: 576px) {
