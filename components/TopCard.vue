4d6cd91e2144a001f9884eb704295705dfd638b3
Imanol Romera <imanolrrl99@gmail.com>
2023-12-23 14:34:19 +0100

    Small improvements to styling

--- components/TopCard.vue
+++ components/TopCard.vue
@@ -21,34 +21,12 @@ div.card {
 }
 
 img {
+  margin: 2em auto;
   width: 100%;
+  max-width: 160px;
   object-fit: contain;
-  animation-duration: 30s;
-  animation-name: animimg;
-  animation-direction: normal;
-  animation-iteration-count: infinite;
-  animation-timing-function: ease-in-out;
-}
-
-@keyframes animimg {
-  0% {
-    border-radius: 40% 60% 70% 30% / 46% 30% 70% 54%;
-  }
-  20% {
-    border-radius: 66% 34% 53% 47% / 44% 59% 41% 56%;
-  }
-  40% {
-    border-radius: 56% 44% 44% 56% / 68% 49% 51% 32%;
-  }
-  60% {
-    border-radius: 50% 50% 48% 52% / 48% 61% 39% 52%;
-  }
-  80% {
-    border-radius: 61% 39% 27% 73% / 67% 40% 60% 33%;
-  }
-  100% {
-    border-radius: 40% 60% 70% 30% / 46% 30% 70% 54%;
-  }
+  border: 3px solid var(--text);
+  border-radius: 999vw;
 }
 
 @media (min-width: 576px) {
@@ -61,10 +39,8 @@ img {
   }
 
   img {
-    margin-left: auto;
-    width: 50%;
-    max-width: 44ch;
-    object-fit: contain;
+    width: 33%;
+    max-width: 260px;
   }
 }
 </style>
